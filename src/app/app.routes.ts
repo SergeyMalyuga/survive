1b372f5b6d1519da5b4b804fe8f38a18007fb8c0
Sergey Malyuga <sergey@malyuga.ru>
2025-11-20 19:48:17 +0300

    Установить prettier, stylelint scss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AppRoutes} from './core/constants/const';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: AppRoutes.MAIN,
+    title: 'Survive at all costs',
+    loadComponent: () => import('./pages/main/main.component').then(m => m.MainComponent)
+  }
+];
