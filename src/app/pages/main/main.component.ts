bfbab977d24742f99ef39bb216d37a13e2d37bc0
Sergey Malyuga <sergey@malyuga.ru>
2025-11-20 23:19:18 +0300

    Создать header.component

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {HeaderComponent} from '../../features/header/header.component';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [
+    HeaderComponent
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
