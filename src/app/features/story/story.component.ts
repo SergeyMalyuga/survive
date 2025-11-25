2e8969949f30ac12cce729dce0d1e4eacbbda173
Sergey Malyuga <sergey@malyuga.ru>
2025-11-25 19:52:26 +0300

    Добавить swiper в story.component

--- src/app/features/story/story.component.ts
+++ src/app/features/story/story.component.ts
@@ -1,10 +1,13 @@
 import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output, signal} from '@angular/core';
 import {ScrollToStoryDirective} from './directives/scroll-to-story.directive';
+import {SwiperComponent} from '../swiper/swiper.component';
+import {SwiperType} from '../../core/constants/const';
 
 @Component({
   selector: 'app-story',
   imports: [
-    ScrollToStoryDirective
+    ScrollToStoryDirective,
+    SwiperComponent
   ],
   templateUrl: './story.component.html',
   styleUrl: './story.component.scss',
@@ -20,4 +23,6 @@ export class StoryComponent {
   get isStoryScroll() {
     return this._isStoryScroll;
   }
+
+  protected readonly SwiperType = SwiperType;
 }
