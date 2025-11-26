72b62fe9b1ccb097697e3343c361a5d6c2ec1549
Sergey Malyuga <sergey@malyuga.ru>
2025-11-26 11:45:15 +0300

    Создать details.component

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,13 +2,15 @@ import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 import {HeaderComponent} from '../../features/header/header.component';
 import {HeroComponent} from '../../features/hero/hero.component';
 import {StoryComponent} from '../../features/story/story.component';
+import {DetailsComponent} from '../../features/details/details.component';
 
 @Component({
   selector: 'app-main',
   imports: [
     HeaderComponent,
     HeroComponent,
-    StoryComponent
+    StoryComponent,
+    DetailsComponent
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
