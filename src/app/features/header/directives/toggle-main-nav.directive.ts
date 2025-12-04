7ded19ff1fde9ec34fcd7b579c1376ead0859cb4
Sergey Malyuga <sergey@malyuga.ru>
2025-12-04 09:34:25 +0300

    Реализовать закрытие меню навигации по сайту при нажатии Escape

--- src/app/features/header/directives/toggle-main-nav.directive.ts
+++ src/app/features/header/directives/toggle-main-nav.directive.ts
@@ -5,12 +5,23 @@ import {BodyService} from '../../../core/services/body.service';
   selector: '[appToggleMainNav]',
 })
 export class ToggleMainNavDirective {
-  @Input({required:true}) isMainNavOpen!: boolean;
+  @Input({required: true}) isMainNavOpen!: boolean;
   @Output() mainNavToggled = new EventEmitter<void>();
   private bodyService = inject(BodyService);
 
   @HostListener('click')
   handleMainNavBtnClick() {
+    this.toggleNavMenu()
+  }
+
+  @HostListener('window:keydown.escape', ['$event'])
+  onKeyDown(evt: KeyboardEvent) {
+    if (evt.key === 'Escape' && this.isMainNavOpen) {
+      this.toggleNavMenu();
+    }
+  }
+
+  private toggleNavMenu() {
     this.mainNavToggled.emit();
     this.bodyService.setOverflow(!this.isMainNavOpen);
   }
