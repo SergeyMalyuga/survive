7e8808a24b3cda28dabb6cbad7d634b55b0debe9
Sergey Malyuga <sergey@malyuga.ru>
2025-12-09 13:49:32 +0300

    Добавить языковой перевод для subscribe.component

--- src/app/features/subscribe/subscribe.component.ts
+++ src/app/features/subscribe/subscribe.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-subscribe',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './subscribe.component.html',
   styleUrl: './subscribe.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
