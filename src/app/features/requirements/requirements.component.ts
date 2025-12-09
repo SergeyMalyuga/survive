502309c306d614c1e67c4e7b6e9062c421540481
Sergey Malyuga <sergey@malyuga.ru>
2025-12-09 13:13:18 +0300

    Добавить языковой перевод для requirements.component

--- src/app/features/requirements/requirements.component.ts
+++ src/app/features/requirements/requirements.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-requirements',
   templateUrl: './requirements.component.html',
   styleUrl: './requirements.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [TranslatePipe]
 })
 export class RequirementsComponent {}
