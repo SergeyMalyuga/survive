b12b0a02544d95c3dc12407f063455c707158887
Sergey Malyuga <sergey@malyuga.ru>
2025-12-11 12:25:55 +0300

    Добавить перевод для footer.component

--- src/app/features/footer/footer.component.ts
+++ src/app/features/footer/footer.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
